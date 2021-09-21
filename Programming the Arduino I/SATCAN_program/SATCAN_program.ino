c53f955df032d1e719b575a45191fdf6442933e2
pradyu152002 <61514283+pradyu152002@users.noreply.github.com>
2021-09-21 08:21:07 +0530

    Update SATCAN_program.ino

--- Programming the Arduino I/SATCAN_program/SATCAN_program.ino
+++ Programming the Arduino I/SATCAN_program/SATCAN_program.ino
@@ -26,6 +26,12 @@ void setup() {
     Serial.println("BMP not initialised....");
     while(1);
   }
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   
   referencePressure = bmp.readPressure();
 }
